4e8e5be6f5dd51eccc9c9ed69f951d1eff41ae67
Sathana-M-Offi <sathanamurugan29@gmail.com>
2025-10-17 05:48:28 +0530

    NM 2025

---
+++ java script basic/Simple.js
@@ -0,0 +1,68 @@
+var apple=10;
+var totalKG=5;
+var totalAmount=apple*totalKG;
+console.log("The 5 kg Apple rate is:",totalAmount);
+
+console.log("Decrement Operation")
+var o=250;
+var k=o--;
+console.log("The Value of O is:",o);
+console.log("THe value of K is:",k);
+
+
+console.log("Increment Operation")
+var m=500;
+var e=m++;
+console.log("The Value of M is:",m);
+console.log("The Value of E is:",e);
+
+
+console.log("Addition Operation")
+var a=1000;
+var b=300;
+var i=a+b;
+var n=a-b;
+var r=a*b;
+var q=a/b;
+var t=a%b;
+console.log("Addition of a and b is:",i);
+console.log("Subtraction of a and b is:",n);
+console.log("Multiplication of a and b is:",r);
+console.log("Division of a and b is:",q);
+console.log("Modulus of a and b is:",t);
+
+
+console.log("Comparison of a and b");
+if(a==b)
+{
+    console.log("It is matched");
+}
+else
+{
+    console.log("Not matched");
+}
+
+
+console.log("Comparison of a and b")
+if (a>b)
+{
+    console.log("A is Greater than B:",a);
+}
+else
+{
+    console.log("B is Greater than A:",b)
+}
+
+
+var f=1000
+var h=+-f;
+console.log("The Value of F:",f);
+console.log("The Value of H is:",h);
+console.log("The Value of F is:",f); 
+
+
+
+
+
+ 
+
